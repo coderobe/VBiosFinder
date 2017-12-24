454aa4a09cd89ac4c8834f4e0a1675bc6b223f96
Robin Broda <robin@broda.me>
2017-12-24 13:18:14 +0100

    Add helper functions

---
+++ src/utils.rb
@@ -0,0 +1,23 @@
+require "cocaine"
+require "mkmf"
+require "find"
+
+module VBiosFinder
+  class Utils
+    @@current_files = []
+    def self.get_new_files
+      current_files = Find.find(".").reject{|e| File.directory? e}
+      result = current_files - @@current_files
+      @@current_files = current_files
+      return result
+    end
+    def self.installed? program, reason="optional"
+      if find_executable(program).nil?
+        puts "Install '#{program}' on your system (#{reason})"
+        false
+      else
+        true
+      end
+    end
+  end
+end
