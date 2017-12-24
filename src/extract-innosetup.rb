c49504435cdeefacaa459e8cf0114ceeea42e1d4
Robin Broda <robin@broda.me>
2017-12-24 18:08:44 +0100

    Remove debug message

--- src/extract-innosetup.rb
+++ src/extract-innosetup.rb
@@ -3,7 +3,6 @@ require "cocaine"
 module VBiosFinder
   class Extract
     def self.innosetup file
-      puts file
       begin
         line = Cocaine::CommandLine.new("innoextract", ":file")
         puts line.run(file: file)
